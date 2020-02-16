e6aebba4988aef2086b7ffbe10237d11c69ebc7a
Volkmar Rigo <volkmar@rigo.bz>
2020-02-16 17:49:27 +0100

    Changed interface to abstract class. Added properties for input and output

---
+++ GoogleHashCode/Base/BaseSolver.cs
@@ -0,0 +1,11 @@
+﻿namespace GoogleHashCode.Base
+{
+	public abstract class BaseSolver<TInput, TOutput> where TInput : IInput, new() where TOutput : IOutput, new()
+	{
+		public TInput Input { get; } = new TInput();
+
+		public TOutput Output { get; } = new TOutput();
+
+		public abstract void Solve();
+	}
+}
--- GoogleHashCode/Base/ISolver.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GoogleHashCode.Base
-{
-	public interface ISolver<in TInput, out TOutput>
-	{
-		void Solve(TInput input);
-		TOutput GetOutput();
-		int GetScore();
-	}
-}
