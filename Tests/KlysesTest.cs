c3b59d379c9e501b3ff773d703423fdef02269fd
Unknown <Klaus.Pruenster@gmail.com>
2020-02-20 21:22:15 +0100

    solver 3

--- Tests/KlysesTest.cs
+++ Tests/KlysesTest.cs
@@ -22,7 +22,7 @@ namespace Tests
 			example.WriteToFile(output.GetOutputFormat());
 			Assert.Pass();
 		}
-
+		
 		[Test]
 		[TestCase("a_example.txt")]
 		[TestCase("b_read_on.txt")]
@@ -34,6 +34,26 @@ namespace Tests
 		{
 			var content = example.ReadFromFile();
 			var solver = new Solver1();
+			var input = Input.Parse(content);
+			solver.Solve(input);
+			var output = solver.GetOutput();
+
+			Console.WriteLine($"Total Score: {output.GetScore()}");
+			example.WriteToFile(output.GetOutputFormat());
+			Assert.Pass();
+		}
+
+		[Test]
+		[TestCase("a_example.txt")]
+		[TestCase("b_read_on.txt")]
+		[TestCase("c_incunabula.txt")]
+		[TestCase("d_tough_choices.txt")]
+		[TestCase("e_so_many_books.txt")]
+		[TestCase("f_libraries_of_the_world.txt")]
+		public void Solver3(string example)
+		{
+			var content = example.ReadFromFile();
+			var solver = new Solver3();
 			var input = Input.Parse(content);
 			solver.Solve(input);
 			var output = solver.GetOutput();
