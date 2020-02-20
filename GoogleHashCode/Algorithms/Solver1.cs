e252af2c7f5c584eef96bb6dad56d2c19f57b592
Unknown <Klaus.Pruenster@gmail.com>
2020-02-20 20:56:48 +0100

    minor improvements

--- GoogleHashCode/Algorithms/Solver1.cs
+++ GoogleHashCode/Algorithms/Solver1.cs
@@ -13,16 +13,31 @@ namespace GoogleHashCode.Algorithms
 		{
 			Out.Input = input;
 
-			foreach (var library in input.Libraries.OrderBy(c => c.SignupDays))
+			var realBookScore = input.Libraries.SelectMany(c => c.BookIds.ToList()).GroupBy(c => c)
+									 .Select(c => new
+												  {
+													  Id = c.Key,
+													  Score = (input.BookScores.Length - c.Count()) * input.BookScores[c.Key]
+												  })
+									 .ToDictionary(c => c.Id, c => c.Score);
+
+			foreach (var library in input.Libraries)
 			{
 				Out.Libraries.Add(new LibraryAction
 								  {
-									  BookIDs = library.BookIds.ToList(),
+									  BookIDs = library.BookIds.OrderByDescending(c => realBookScore[c]).ThenByDescending(c => input.BookScores[c]).ToList(),
 									  ID = library.Id,
 									  BooksPerDay = library.BooksPerDay,
 									  SignupDays = library.SignupDays
 								  });
 			}
+
+			Out.Libraries = Out.Libraries
+							   //.OrderByDescending(c => c.BookIDs
+										//			   .Select(r => realBookScore[r])
+										//			   .Sum(j => j))
+							   .OrderBy(c => c.SignupDays)
+							   .ToList();
 		}
 
 		public Output GetOutput()
