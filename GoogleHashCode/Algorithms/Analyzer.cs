29df9845f90789175cdfe6db1d107749a6d3d2a9
Volkmar Rigo <volkmar@rigo.bz>
2020-02-20 20:06:11 +0100

    Analyzer

---
+++ GoogleHashCode/Algorithms/Analyzer.cs
@@ -0,0 +1,45 @@
+﻿using GoogleHashCode.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GoogleHashCode.Algorithms
+{
+    public class Analyzer
+    {
+        public StringBuilder Result = new StringBuilder ();
+
+
+
+        private void Analyze(Input input)
+        {
+
+            Result.AppendLine($"Libraries: {input.Libraries.Count}");
+            Result.AppendLine($"MinCount Books: {input.Libraries.Min(q => q.BookIds.Count)}");
+            Result.AppendLine($"MaxCount Books: {input.Libraries.Max(q => q.BookIds.Count)}");
+            Result.AppendLine($"Min Signup Days: {input.Libraries.Min(q => q.SignupDays)}");
+            Result.AppendLine($"Max Signup Days: {input.Libraries.Max(q => q.SignupDays)}");
+
+            var uniqueBooks = new HashSet<int>();
+            foreach (var item in input.Libraries)
+                foreach (var book in item.BookIds)
+                    uniqueBooks.Add(book);
+
+            Result.AppendLine($"UniqueBooks: {uniqueBooks.Count}");
+        }
+
+        public void Execute(List<string> filenames)
+        {
+
+            foreach (var item in filenames)
+            {
+                Result.AppendLine(item);
+                Analyze(Input.Parse(item.ReadFromFile()));
+                Result.AppendLine("--");
+            }
+
+            "Analyze.txt".WriteToFile(Result.ToString());
+        }
+    }
+}
--- Tests/VolkmarsTest.cs
+++ Tests/VolkmarsTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using GoogleHashCode;
 using GoogleHashCode.Algorithms;
 using GoogleHashCode.Model;
@@ -6,26 +7,41 @@ using NUnit.Framework;
 
 namespace Tests
 {
-	public class VolkmarsTest
-	{
-		[Test]
-		[TestCase("a_example.txt")]
-		[TestCase("b_read_on.txt")]
-		[TestCase("c_incunabula.txt")]
-		[TestCase("d_tough_choices.txt")]
-		[TestCase("e_so_many_books.txt")]
-		[TestCase("f_libraries_of_the_world.txt")]
-		public void Solver1(string example)
-		{
-			var content = example.ReadFromFile();
-			var solver = new Solver2();
-			var input = Input.Parse(content);
-			solver.Solve(input);
-			var output = solver.GetOutput();
+    public class VolkmarsTest
+    {
+        [Test]
+        [TestCase("a_example.txt")]
+        [TestCase("b_read_on.txt")]
+        [TestCase("c_incunabula.txt")]
+        [TestCase("d_tough_choices.txt")]
+        [TestCase("e_so_many_books.txt")]
+        [TestCase("f_libraries_of_the_world.txt")]
+        public void Solver1(string example)
+        {
+            var content = example.ReadFromFile();
+            var solver = new Solver2();
+            var input = Input.Parse(content);
+            solver.Solve(input);
+            var output = solver.GetOutput();
 
-			Console.WriteLine($"Total Score: {output.GetScore()}");
-			example.WriteToFile(output.GetOutputFormat());
-			Assert.Pass();
-		}
-	}
+            Console.WriteLine($"Total Score: {output.GetScore()}");
+            example.WriteToFile(output.GetOutputFormat());
+            Assert.Pass();
+        }
+
+
+        [Test]
+        public void Analyze()
+        {
+            new Analyzer().Execute(new List<string>
+            {
+                "a_example.txt",
+                "b_read_on.txt", "c_incunabula.txt", "d_tough_choices.txt", "e_so_many_books.txt", "f_libraries_of_the_world.txt"
+            });
+
+            Assert.Pass();
+        }
+
+
+    }
 }
